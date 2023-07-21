d7efa5cdc4c61f62651a85c49159e0fa926f1315
EmelKrist <kristina_1516@mail.ru>
2023-07-21 18:34:27 +0800

    ﻿Создан проекта бота
    * Создан микросервис-диспетчер
    * Внедрены необходимые зависимости
    * Добавлен логгер
    * Совершена интеграция с телеграмом

---
+++ dispatcher/src/main/java/ru/emelkrist/dispatcher/controller/TelegramBot.java
@@ -0,0 +1,50 @@
+package ru.emelkrist.dispatcher.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import ru.emelkrist.dispatcher.config.BotConfig;
+
+@Component
+@Slf4j
+public class TelegramBot extends TelegramLongPollingBot {
+
+    private final BotConfig config;
+
+    @Autowired
+    public TelegramBot(BotConfig config) {
+        this.config = config;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return config.getBotToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        var originalMessage = update.getMessage();
+
+        SendMessage message = new SendMessage();
+        message.setChatId(update.getMessage().getFrom().getId());
+        message.setText("Hi from bot!");
+        executeMessage(message);
+    }
+
+    private void executeMessage(SendMessage message) {
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            log.error(e.getMessage());
+        }
+    }
+}
